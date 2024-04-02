6201698640664c7987af340cc3343806e4c5b3ff
Vagner Sousa Ribeiro <vagner.sousa@g.globo>
2024-04-01 21:01:22 -0300

    remove comments

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -5,15 +5,11 @@ var connection = new signalR.HubConnectionBuilder()
     .withAutomaticReconnect([0, 0, 10000])
     .build();
 
-//Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
     li.textContent = `${user} says ${message}`;
 });
 
